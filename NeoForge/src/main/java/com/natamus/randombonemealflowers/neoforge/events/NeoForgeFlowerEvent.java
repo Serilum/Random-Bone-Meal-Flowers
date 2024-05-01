3210c07b0c894b1f79819aaffd244bdf1847842d
Rick South <rick@natamus.com>
2024-05-01 16:33:02 +0200

    Random Bone Meal Flowers source code for 1.20.6.

---
+++ NeoForge/src/main/java/com/natamus/randombonemealflowers/neoforge/events/NeoForgeFlowerEvent.java
@@ -0,0 +1,23 @@
+package com.natamus.randombonemealflowers.neoforge.events;
+
+import com.natamus.collective.functions.WorldFunctions;
+import com.natamus.randombonemealflowers.events.FlowerEvent;
+import com.natamus.randombonemealflowers.util.Util;
+import net.minecraft.world.level.Level;
+import net.neoforged.neoforge.event.entity.player.BonemealEvent;
+import net.neoforged.neoforge.event.level.LevelEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+
+@EventBusSubscriber
+public class NeoForgeFlowerEvent {
+	@SubscribeEvent
+	public static void onWorldLoad(LevelEvent.Load e) {
+		Level level = WorldFunctions.getWorldIfInstanceOfAndNotRemote(e.getLevel());
+		if (level == null) {
+			return;
+		}
+
+		Util.attemptFlowerlistProcessing(level);
+	}
+}
